f2e5afe64bbd2e6a13a733386330b8d3b709b989
0xJiros <ariyantowibisanaarrasyid01@gmail.com>
2025-04-24 17:59:22 +0700

    add tasks.json dan perubahan pada  app.js

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,58 @@
-console.log("Hello World");
+// let namaDepan = "Ariyanto";
+// let favoriteFood = "gogo";
+// let namaLengkap = `Halo nama saya ${namaDepan} dan makan favorit saya ${favoriteFood}`;
+// console.log(namaLengkap);
+
+// let apel = 5000,
+//   pisang = 10000;
+// let jumlahApel = 3,
+//   jumlahPisang = 2;
+// // let c = apel + apel + apel + pisang + pisang - 10000;
+// let c = apel * jumlahApel + pisang * jumlahPisang - 10000;
+// console.log(c);
+
+// let keranjang = [
+//   { nama: "apel", harga: 5000, qty: 3 },
+//   { nama: "pisang", harga: 10000, qty: 2 },
+// ];
+// let total = keranjang.reduce((acc, item) => acc + item.harga * item.qty, 0);
+
+// let diskon = total * 0.1;
+// let totalAkhir = total - diskon;
+// console.log(totalAkhir);
+
+// const age = 17;
+// const hasSim = false;
+// const isTest = false;
+
+// if (isTest || (age >= 18 && hasSim)) {
+//   console.log("boleh mengemudi");
+// } else if (age >= 18 && !hasSim) {
+//   console.log("bikin sim dulu");
+// } else if (age < 18 && hasSim) {
+//   console.log("boleh mengemudi dengan syarat");
+// } else {
+//   console.log("pulang aja dek");
+// }
+
+// const isDrunk = true;
+// const canDrive = !isDrunk;
+// console.log(canDrive ? "Bisa mengemudi" : "Tidak bisa mengemudi");
+
+// const age = 15;
+// const withParents = true;
+// const minAge = 17;
+
+// // if (age > 17) {
+// //   console.log("Boleh nonton");
+// // }
+// // else if (age < 17 && withParents) {
+// //   console.log("Boleh nonton dengan orang tua");
+// // }
+// // else {
+// //     console.log("Tidak boleh nonton");
+// // }
+
+// console.log(age >= minAge ? "Boleh Nonton" : (withParents ? "Boleh Nonton Dengan Orang Tua" : "gaboleh nonton"));
+
+let namaDepan = "Ariyanto";
